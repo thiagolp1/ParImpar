d8f621a4037e29dd92062b3dd3fe1a70abd69521
Thiago <Thiagolopespereiralp1@gmail.com>
2022-04-25 17:33:49 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int numero;
+String paridade;
+
+Console.Write("Digite um numero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+paridade = (numero % 2) == 0 ? "par" : "impar";
+
+Console.WriteLine($"{numero} e {paridade}");
